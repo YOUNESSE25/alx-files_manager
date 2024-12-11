fb2f87350042786583cd70555c9d31aec41f617b
YOUNESSE25 <younessehaouas@gmail.com>
2024-12-12 00:50:01 +0100

    file_manager

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
+import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
 
-const router = express.Router();
+const router = Router();
 
 router.get('/status', AppController.getStatus);
 
